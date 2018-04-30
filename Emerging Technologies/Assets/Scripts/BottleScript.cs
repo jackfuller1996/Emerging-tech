3671b0d4062ffb428565f78b63f5d5bb64ff5afe
jackfuller1996 <s176802@ucs.ac.uk>
2018-04-30 03:39:41 +0100

    Assignment

---
+++ Emerging Technologies/Assets/Scripts/BottleScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap;
+using Leap.Unity;
+
+public class BottleScript : MonoBehaviour {
+
+    float timeLeft = 5.0f;
+    public int scoreValue = 10;
+
+    void Start()
+    {
+    }
+	// Update is called once per frame
+	void Update () {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        HandModel hand = collision.gameObject.GetComponentInParent<HandModel>();
+        if (hand != null)
+        {
+            ScoreManager.score += scoreValue;
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Emerging Technologies/Assets/Scripts/Spawner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+    public GameObject BottlePrefab;
+
+    void Start()
+    {
+        GameObject bottleSpawn = (GameObject)Instantiate(BottlePrefab);
+    }
+
+}
